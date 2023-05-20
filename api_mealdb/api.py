ae2368af4820c31d0cd563dbb462568831182774
Serhii Afonin <serhij.afonin@gmail.com>
2023-05-20 12:37:29 -0300

    add: api low function to follow /low telegram command

--- api_mealdb/api.py
+++ api_mealdb/api.py
@@ -1,7 +1,7 @@
-from utils.helpers import SettingsApi
+from utils.helpers import SettingsApi, my_zip
 from typing import Optional, Dict
 import requests
-
+from pprint import pprint
 
 api_sets = SettingsApi()
 
@@ -11,8 +11,8 @@ headers = {
 }
 
 
-def make_response(action: str, headers: Dict[str, str]=headers, params: Optional[Dict] = None) -> list:
-    url = f"https://themealdb.p.rapidapi.com/{action}.php"
+def make_response(url_action: str, headers: Dict[str, str]=headers, params: Optional[Dict] = None) -> list:
+    url = f"https://themealdb.p.rapidapi.com/{url_action}.php"
     response = requests.get(url, headers=headers, params=params).json().get("meals")
     return response
 
@@ -43,4 +43,57 @@ def get_meal_by_name(meal_name: str) -> Optional[dict]:
 
 
 def get_random_meal() -> dict:
+    """Returns random meal dict"""
+
     return make_response("random")[0]
+
+
+def get_meals_by_category(category_name) -> list[dict]:
+    """Returns meals list by given category"""
+
+    querystring = {"c": category_name}
+    response: list[dict] = make_response("filter", params=querystring)
+    return response
+
+
+def get_meal_by_id(meal_id) -> dict:
+    """Returns meal's description by id"""
+
+    querystring = {"i": str(meal_id)}
+    response: list = make_response("lookup", params=querystring)
+    return response[0]
+
+
+def get_meal_ingredients(meal_id) -> list[tuple]:
+    """Returns meal ingredients by id"""
+
+    meal = get_meal_by_id(meal_id)
+    ingredients: list = [meal.get(f"strIngredient{ingredient_num}") for ingredient_num in range(1, 21)]
+    measures = [meal.get(f"strMeasure{ingredient_num}") for ingredient_num in range(1, 21)]
+
+    return my_zip(ingredients, measures)
+
+
+def low(category_name):
+    """
+    Returns slice of first 3 meals
+    with the lowest quantity of ingredients by category
+    """
+
+    meals_list = get_meals_by_category(category_name)
+
+    for meal in meals_list:
+        meal_id = meal.get("idMeal")
+        ingredients_list = get_meal_ingredients(meal_id)
+        ingredients_qty = len(ingredients_list)
+        meal["ingredients_qty"] = ingredients_qty
+
+    meals_list.sort(key=lambda meal: meal.get("ingredients_qty"))
+    return meals_list[:3]
+
+
+if __name__ == "__main__":
+    pprint(get_meals_by_category("pork"))
+    pprint(get_meal_by_id(52885))
+    pprint(get_meal_ingredients(52885))
+    pprint(low("pork"))
