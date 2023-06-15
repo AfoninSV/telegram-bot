ac35aff794db8a747f0049d1a036033d1fdeab2c
Serhii Afonin <serhij.afonin@gmail.com>
2023-06-15 20:28:11 -0300

    add conversation handler (low/high commands responsible)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.helpers import api_sets
-from telegram_bot.handlers import start_handler
+from telegram_bot.handlers import start_handler, lh_conversation
 from telegram.ext import ApplicationBuilder
 
 # Initialisation bot app
@@ -7,6 +7,7 @@ application = ApplicationBuilder().token(api_sets.tg_token.get_secret_value()).b
 
 # Add handlers to app
 application.add_handler(start_handler)
+application.add_handler(lh_conversation)
 
 # Run the app
 application.run_polling()
