56451b2c465dc3e114aea564d59c5d40b9765d6b
Serhii Afonin <serhij.afonin@gmail.com>
2023-06-15 20:57:42 -0300

    add conversation handler (low/high commands responsible)

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -1,5 +1,24 @@
-from .commands import start
-from telegram.ext import CommandHandler
+from .commands import start, ask_category, low_reply, high_reply, cancel, button
+from telegram.ext import (CommandHandler, ConversationHandler,
+                          MessageHandler, CallbackQueryHandler,
+                          filters)
+from warnings import filterwarnings
+from telegram.warnings import PTBUserWarning
 
+#filterwarnings(action="ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning)
 
 start_handler = CommandHandler('start', start)
+
+lh_conversation = ConversationHandler(
+    entry_points=[CommandHandler("low", ask_category),
+                  CommandHandler("high", ask_category)],
+    states={
+        0: [MessageHandler(filters.TEXT, cancel)],
+        # For /low, /high commands
+        1: [MessageHandler(filters.TEXT, low_reply)],
+        2: [MessageHandler(filters.TEXT, high_reply)],
+        3: [CallbackQueryHandler(button)]
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+    per_user=True
+)
