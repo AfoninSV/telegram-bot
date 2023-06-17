42a9b76dc63a96cbeadf09ac7160ec22d69149f6
Serhii Afonin <serhij.afonin@gmail.com>
2023-06-17 14:15:45 -0300

    add package config_data

---
+++ config_data/config.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+from pydantic import SecretStr, StrictStr, BaseSettings
+
+if not find_dotenv():
+    exit("No '.env' file found.")
+
+env_path = Path(__file__).parent.parent.joinpath(".env")
+load_dotenv(env_path)
+
+
+class SettingsApi(BaseSettings):
+    meal_api_key: SecretStr = os.getenv("API_KEY", None)
+    meal_api_host: StrictStr = os.getenv("API_HOST", None)
+    tg_token: SecretStr = os.getenv("TG_TOKEN")
+
+
+api_settings = SettingsApi()
