a40e1b0f779a4465615ad1dfcae9ca3ef3f4be8d
Serhii Afonin <serhij.afonin@gmail.com>
2023-07-16 14:16:26 -0300

    test

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ server = Flask(__name__)
 
 @server.route('/' + token, methods=['POST'])
 def get_message():
-    json_string = request.get_data().decode('utf-8')
+    json_string = request.stream.read().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
