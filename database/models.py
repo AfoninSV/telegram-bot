828381370fbddd122c3597fe899961d345ef19b7
Serhii Afonin <serhij.afonin@gmail.com>
2023-08-11 20:04:55 -0300

    user, meal, favorites joined into one db

--- database/models.py
+++ database/models.py
@@ -1,27 +1,22 @@
-from datetime import datetime
-
 import peewee as pw
 
 
-db_user = pw.SqliteDatabase("user.db")
+db = pw.SqliteDatabase("database.db")  # for User, Meal, Favorites
 db_history = pw.SqliteDatabase("history.db")
-db_meal = pw.SqliteDatabase("meal.db")
 db_states = pw.SqliteDatabase("states.db")
-db_fridge = pw.SqliteDatabase("fridge.db")
-db_favorites = pw.SqliteDatabase("favorites.db")
 
 
 class BaseModel(pw.Model):
-    id_key = pw.AutoField()
+    id = pw.AutoField()
+
+    class Meta:
+        database = db
 
 
 class User(BaseModel):
     """Stores user data"""
 
     user_id = pw.TextField()
-
-    class Meta:
-        database = db_user
 
 
 class Meal(BaseModel):
@@ -33,53 +28,24 @@ class Meal(BaseModel):
     meal_id = pw.TextField()
     title = pw.TextField()
     ingredients = pw.TextField()
-    ingredients_qty = pw.IntegerField(null=True)
-
-    class Meta:
-        database = db_meal
+    ingredients_qty = pw.IntegerField()
 
 
 class Favorites(BaseModel):
     """
-    Stores user - meal relations as favorites
+    Stores user - meal relations as 'favorites'
     """
 
     user = pw.ForeignKeyField(User, backref="favorites")
     meal = pw.ForeignKeyField(Meal, backref="favorites")
 
-    class Meta:
-        database = db_favorites
 
-
-class States(BaseModel):
-    """
-    Stores states of users
-    """
-
-    user_id = pw.IntegerField()
-    state = pw.IntegerField()
-
-    class Meta:
-        database = db_states
-
-
-class History(BaseModel):
+class History:
     """Stores user commands call history"""
 
+    id = pw.AutoField()
     user_id = pw.IntegerField()
     message = pw.TextField()
 
     class Meta:
         database = db_history
-
-
-class Fridge(BaseModel):
-    """
-    Stores ingredients of user's fridge
-    """
-
-    user_id = pw.IntegerField()
-    ingredients = pw.TextField()
-
-    class Meta:
-        database = db_fridge
