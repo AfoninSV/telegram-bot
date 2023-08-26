adec25a54f901a0555e4540672f9c55a0622e789
AfoninSV <107323999+AfoninSV@users.noreply.github.com>
2023-08-26 09:33:24 -0300

    cleaned unused imports

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-from config_data.config import api_settings
-import telebot
 from telebot.storage import StateMemoryStorage
-from flask import Flask, request
-
-import os
 
 from telegram_bot import handlers
 from utils.bot_commands import set_commands
 from loader import bot
 
 storage = StateMemoryStorage()
-token = api_settings.tg_token.get_secret_value()
-
-# Initialization
-server = Flask(__name__)
-
-@server.route('/', methods=['POST'])
-def getMessage():
-    json_string = request.stream.read().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    print(bot.get_webhook_info())
-    return f"{bot.get_webhook_info()}", 200
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    if bot.set_webhook(url='https://tg-tasty-bot-2f2c1b337730.herokuapp.com/'):
-        return "Webhook set up successfully!", 200
-    else:
-        return "Webhook setup failed!", 500
 
 set_commands(bot)
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    bot.infinity_polling()
